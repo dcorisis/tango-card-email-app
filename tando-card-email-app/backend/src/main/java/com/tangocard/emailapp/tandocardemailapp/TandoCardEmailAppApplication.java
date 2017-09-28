afeaaa406ae07ed36b563ba8884f0c59b52a56ea
dcorisis <djcorisis@gmail.com>
2017-09-27 18:30:39 -0600

    adding base project built for Java Spring with Angular 2

---
+++ tando-card-email-app/backend/src/main/java/com/tangocard/emailapp/tandocardemailapp/TandoCardEmailAppApplication.java
@@ -0,0 +1,12 @@
+package com.tangocard.emailapp.tandocardemailapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TandoCardEmailAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TandoCardEmailAppApplication.class, args);
+	}
+}
---
+++ tando-card-email-app/backend/src/test/java/com/tangocard/emailapp/tandocardemailapp/TandoCardEmailAppApplicationTests.java
@@ -0,0 +1,16 @@
+package com.tangocard.emailapp.tandocardemailapp;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TandoCardEmailAppApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
